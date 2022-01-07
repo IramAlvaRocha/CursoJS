b57e56faa577d04a426ff3d05267ac9bc437bc49
Iram Alvarez Rocha <iram.alvarezrch@gmail.com>
2022-01-07 13:44:28 -0600

    Ejercicios de Lógica de Programación 1-9

---
+++ js/constantes.js
@@ -0,0 +1,18 @@
+export const PI = Math.PI;
+
+export let usuario = "Iram";
+
+let password = "qwerty";
+// export default password;
+
+const hello = () => console.log("Hello");
+
+export default function saludar() {
+  console.log("Hola mundo +ES6");
+}
+
+export class Saludar {
+  constructor() {
+    console.log("Hola mundo +ES6");
+  }
+}
---
+++ js/modulos.js
@@ -0,0 +1,11 @@
+import saludar, { Saludar, PI, usuario } from "./constantes.js";
+import { aritmetica } from "./aritmetica.js";
+
+console.log(aritmetica.sumar(3, 8));
+console.log(aritmetica.restar(3, 8));
+saludar();
+
+let saludo = new Saludar();
+console.log(saludo);
+saludo;
+s;
